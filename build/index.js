ad5ab7c514163fcbc113a2095da2a4e0c3033c6d
Hyde Stefan <79136747+DrHyde01@users.noreply.github.com>
2023-07-26 21:06:51 +0200

    Add conditions & style

--- build/index.js
+++ build/index.js
@@ -20,6 +20,24 @@ document.querySelector('button').onclick = function () {
     let imc = (weight) / Math.pow(height, 2);
     let imcRound = imc.toFixed(1);
     let imcOutput = document.querySelector('.imc');
+    let imcMessage = document.querySelector('.imc-message');
+    imcMessage.setAttribute('style', 'display: flex; justify-content: center');
+    if (imc < 18.5) {
+        imcOutput.classList.add('imc-blue');
+        imcOutput.classList.remove('imc-yellow', 'imc-red', 'imc-green');
+    }
+    else if (imc > 18.5 && imc < 24.5) {
+        imcOutput.classList.add('imc-green');
+        imcOutput.classList.remove('imc-blue', 'imc-yellow', 'imc-red');
+    }
+    else if (imc > 24.5 && imc < 29.9) {
+        imcOutput.classList.add('imc-yellow');
+        imcOutput.classList.remove('imc-green', 'imc-red', 'imc-blue');
+    }
+    else if (imc > 29.9 && imc < 1000) {
+        imcOutput.classList.add('imc-red');
+        imcOutput.classList.remove('imc-green', 'imc-yellow', 'imc-blue');
+    }
     imcOutput.textContent = imcRound.toString();
 };
 displayValues();
--- src/index.ts
+++ src/index.ts
@@ -26,8 +26,31 @@ document.querySelector('button').onclick = function () :void {
     let imcRound :string = imc.toFixed(1);
 
     let imcOutput :HTMLElement = document.querySelector('.imc');
+    let imcMessage :HTMLElement = document.querySelector('.imc-message');
+
+    imcMessage.setAttribute('style', 'display: flex; justify-content: center');
+
+    if (imc < 18.5) {
+        imcOutput.classList.add('imc-blue');
+        imcOutput.classList.remove('imc-yellow', 'imc-red', 'imc-green');
+
+    }
+    else if (imc > 18.5 && imc < 24.5 ) {
+        imcOutput.classList.add('imc-green')
+        imcOutput.classList.remove('imc-blue', 'imc-yellow', 'imc-red');
+    }
+    else if (imc > 24.5 && imc < 29.9) {
+        imcOutput.classList.add('imc-yellow');
+        imcOutput.classList.remove('imc-green', 'imc-red', 'imc-blue');
+    }
+    else if (imc > 29.9 && imc < 1000) {
+        imcOutput.classList.add('imc-red');
+        imcOutput.classList.remove( 'imc-green','imc-yellow', 'imc-blue');
+    }
+
 
     imcOutput.textContent = imcRound.toString();
+
 }
 
 displayValues();
